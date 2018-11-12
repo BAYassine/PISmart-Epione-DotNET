228df43ebd096dd3117ce13af90faee271cf508e
Bayassine <lomyassen@gmail.com>
2018-11-12 14:57:54 +0100

    auth missing files

---
+++ Presentation/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using Domain.Entities;
+using Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class AuthController : Controller
+    {
+        static HttpClient client = new HttpClient();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(string username, string password)
+        {
+            var credtls = new Dictionary<string, string>();
+            credtls.Add("username", username);
+            credtls.Add("password", password);
+            var response = await client.PostAsync("http://localhost:18080/epione-jee-web/api/auth", new FormUrlEncodedContent(credtls));
+            response.EnsureSuccessStatusCode();
+            Session["authtoken"] = response.Content.ReadAsStringAsync().Result;
+            //System.Diagnostics.Debug.WriteLine("token : " + Session["authtoken"]);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("authtoken");
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> register([Bind(Include = "username,email,password,confirmPassword,role")] UserVM uservm)
+        {
+            if (ModelState.IsValid)
+            {
+                var u = new 
+                {
+                    username = uservm.username,
+                    email = uservm.email,
+                    password = uservm.password,
+                    role = uservm.role.ToString()
+                };
+                HttpResponseMessage response = await client.PostAsJsonAsync("http://localhost:18080/epione-jee-web/api/users/register", u);
+                var contents = response.Content.ReadAsStringAsync().Result;
+                response.EnsureSuccessStatusCode();
+                return View("Greeting");
+            }
+
+            return View(uservm);
+        }
+
+    }
+}
---
+++ Presentation/Models/UserVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.Models
+{
+    public enum Roles {
+        [Display(Name ="Doctor")]
+        ROLE_DOCTOR,
+        [Display(Name ="Patient")]
+        ROLE_PATIENT
+    };
+
+    public class UserVM
+    {
+
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Username")]
+        public string username { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string password { get; set; }
+
+        [Compare("password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Re-enter Password")]
+        public string confirmPassword { get; set; }
+        
+        [Display(Name = "Account type")]
+        public Roles role { get; set; }
+    }
+}
